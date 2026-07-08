a9513cdbf78ce2dd8f4fca487b2ed51943dff1bc
Moulya Meruva <moulyameruva6@gmail.com>
2026-07-08 17:55:31 +0530

    Data types

---
+++ python filee/opp.py
@@ -0,0 +1,82 @@
+print("Arithmetic Operator:")
+a=13
+b=23
+print("Addition",a+b)
+print("substraction",a-b)
+print("Multiplication",a*b)
+print("Division",a/b)
+print("Floor Division",a//b)
+print("Modular Division",a%b)
+print("Exponential",a**2)
+
+print("Assignment Operators:")
+a=12
+a+=2
+print("Assignment Addition",a)
+a_=2
+print("Assignment Subtraction",a)
+a*=2
+print("Assignment Multiplication",a)
+a/=2
+print("Assignment Division",a)
+a//=2
+print("Assignment FloorDivision",a)
+a%=2
+print("Assignment ModularDivision",a)
+a**=2
+print("Assignment Exponential",a)
+
+print("Relational Operators:")
+a=35
+b=20
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+
+print("Logical Operators:")
+a=10>-10
+b=5!=-5
+print(a and b)
+a=12/2>5
+b=11<=11
+print(a or b)
+a=10<4
+print(not a)
+
+print("Bitwise Operator:")
+a=10
+b=5
+print(a&b)
+a=8
+b=3
+print(a|b)
+a=9
+b=5
+print(a^b)
+a=12<<2
+print(a)
+a=98>>3
+print(a)
+
+print("Membereship Operator:")
+m1=[1,3,5,7]
+print(6 in m1)
+print(3 in m1)
+print(9 not in m1)
+
+print("Identity Operator:")
+a=25
+b=14
+print(a is b)
+a=15
+b=a
+print(a is b)
+print(a is not b)
+
+
+
+
+
